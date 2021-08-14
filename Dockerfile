523c6fec3cbddfcc5e5a5af61aa91686b447391c
sevmac <sivam.evt@gmail.com>
2021-08-14 21:44:37 +0300

    Added a simple flask server

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM python:3
 
 WORKDIR /usr/src/app/
+COPY 'main.py' /usr/src/app/
+COPY 'requirements.txt' /usr/src/app/
+RUN mkdir -p /usr/src/app/resources/
 
-COPY . /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+ENV TZ Europe/Kiev
+
+EXPOSE 8010
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Hello world!")
+from datetime import datetime
+import os
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+if not os.path.isfile('resources/logs.json'):
+    print('Creating new json file...')
+
+    with open('resources/logs.json', 'w') as f:
+        text_dict = {'main_text': "Test text",
+                     'last_text': None,
+                     'connect_log': list()}
+        json.dump(text_dict, fp = f)
+
+    print('...json new file created')
+else:
+    print('Json file already exists ')
+
+i = 0
+
+@app.route('/')
+def main_page():
+    with open('resources/logs.json', 'r') as f:
+        json_obj = json.load(f)
+
+    now = datetime.now()
+    now_str = now.strftime("%d/%m/%Y, %H:%M:%S")
+    json_obj['connect_log'].append(now_str)
+
+    with open('resources/logs.json', 'w') as f:
+        json.dump(json_obj, fp=f)
+        return json_obj
+
+if __name__  == '__main__':
+    app.run(host='0.0.0.0', port='8010', debug=True)
